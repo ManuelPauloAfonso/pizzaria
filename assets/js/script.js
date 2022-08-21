ce6687d4c8bbd9664eaba1bb671df290dbcfb1c9
Manuel Afonso <manuelpaulafonso29@gmail.com>
2022-08-21 05:42:52 -0300

    initial home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,77 @@
-const modal = document.querySelector('modal-container')
-const abrirModal = document.querySelector('.login-button')
-const fechar = document.querySelector('.fechar')
+document.querySelectorAll('#modal-produto').forEach(btn => 
+    (btn.addEventListener('Clik', () => {
+        const idProduto = btn.getAttribute('data-idProduto')
+        abrirModal(idProduto)
+    })
+
+    )
+
+)
 
 
+const Produt = [
 
-function abrirModal(event){
-    event.preventDefault();
-    modal.classList.add('ativo', 'modal');
+    {
+        id: 1,
+        nome: 'pizza com 4 queijos',
+        preco: '$ 11,94'
+
+
+    },
+    {
+        id: 2,
+        nome: 'pizza com 4 queijos',
+        preco: '$ 11,94'
+
+
+    },
+    {
+        id: 3,
+        nome: 'pizza com 4 queijos',
+        preco: '$ 11,94'
+
+
+    },
+    {
+        id: 4,
+        nome: 'pizza com 4 queijos',
+        preco: '$ 11,94'
+
+
+    },
+    {
+        id: 5,
+        nome: 'pizza com 4 queijos',
+        preco: '$ 11,94'
+
+
+    },
+    {
+        id: 6,
+        nome: 'pizza com 4 queijos',
+        preco: '$ 11,94'
+
+
+    },
+
+]
+
+
+function abrirModal(idProduto){
+
+    const produto = produto.filter(produto => produto.id === idProduto)[0]
+    const modal = document.querySelector('.modal-container')
+    modal.style.display = 'block';
 
 
 }
 
-function fecharModal(event){
-    event.preventDefault();
-    modal.classList.add('ativo')
+function fecharModal(){
+   
+    const fechar = document.querySelector('.modal-container')
+
+    fechar.style.display = 'none'
+
 }
 
 
-botaoFechar.addEventListener('click', fecharModal);
-botaonAbrir.addEventListener('click', abrirModal);
-
-
-console.log('Olá')
